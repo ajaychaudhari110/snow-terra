7f77c34736f43ff818c044f7c5b908d0b0c150e4
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2025-08-08 16:10:12 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "snowflake" {
   account   = var.snowflake_account
-  username  = var.snowflake_user
+  user  = var.snowflake_user
   password  = var.snowflake_password
   role      = var.snowflake_role
 }
