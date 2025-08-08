e127b57edc031da3761e0e303caa1c02cf7b59af
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2025-08-08 16:07:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "snowflake_account" {}
 variable "snowflake_user" {}
 variable "snowflake_password" {
   sensitive = true
+  default   = "" # Not strictly needed, but avoids prompting in CI
 }
 variable "snowflake_role" {}
 variable "snowflake_warehouse" {}
